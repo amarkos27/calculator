cc93a10fbedc608ca7250799e2f81b373d186bbb
amarkos27 <armarkos27@gmail.com>
2022-11-30 12:11:08 -0500

    Change implementation to use objects
    In the previous implementation, the functionality of the display was
    done purely using functions. This posed a problem of being able to keep
    a working expression to be evalutated. Instead, create objects for the
    calculator and for the individual button.

--- script.js
+++ script.js
@@ -1,61 +1,97 @@
-function constructCalculator(){
-    let numbers = ['Clear', 7, 8, 9, 4, 5, 6, 1, 2, 3, 0, '.'];
-    let operators = ['÷','×','-','+', '='];
-    operators.forEach(operator => {
-        let button = document.createElement('div');
-        button.textContent = operator;
-        if(button.textContent === '='){
-            button.style.borderRadius = '0 0 10px 0';
+function Calculator(){
+    this.numbers = ['Clear', 7, 8, 9, 4, 5, 6, 1, 2, 3, 0, '.'];
+    this.operators = ['÷','×','-','+', '='];
+    this.screen = document.querySelector('.screen');
+    this.expression = '';
+
+    this.fill = function(){
+        this.operators.forEach(operator => {
+            let button = new Button(operator);
+            if(button.textContent === '='){
+                button.style.borderRadius = '0 0 10px 0';
+            }
+            document.querySelector('.operators').appendChild(button);
+        });
+        this.numbers.forEach(number => {
+            let button = new Button(number);
+            button.textContent = number;
+            switch(button.textContent){
+                case 'Clear':
+                    button.style.gridColumn = '1/4';
+                    break;
+                case '0':
+                    button.style.gridColumn = '1/3';
+                    button.style.borderRadius = '0 0 0 10px';
+                    break;
+            }
+            document.querySelector('.numbers').appendChild(button);
+        });
+    };
+    
+    this.display = function(){
+        this.screen.textContent = this.expression;
+    };
+
+    this.evaluate = function(button){
+        if(button.textContent === 'Clear'){
+            this.expression = '';
+        }else if(button.parentNode.classList.contains('numbers')){
+            this.expression += button.textContent;
+        }else{
+            let operatorCheck = /\D/;
+            if(this.expression.split().some(char => operatorCheck.test(char))){
+                console.log('o');
+            }else{
+                this.expression += button.textContent;
+            }
         }
-        document.querySelector('.operators').appendChild(button);
-    });
-    numbers.forEach(number => {
-        let button = document.createElement('div');
-        button.textContent = number;
-        switch(button.textContent){
-            case 'Clear':
-                button.style.gridColumn = '1/4';
-                break;
-            case '0':
-                button.style.gridColumn = '1/3';
-                button.style.borderRadius = '0 0 0 10px';
-                break;
+        this.display();
+    };
+}
+
+function fill(){
+    
+}
+
+function Button(button){
+    this.div = document.createElement('div');
+    this.div.textContent = button;
+    
+    this.div.addEventListener('mousedown', mousedown);
+
+    function mousedown(e){
+        let button = e.target;
+        button.classList.toggle('down');
+        button.addEventListener('mouseleave', mouseleave);
+        window.addEventListener('mouseup', (e) => {
+            mouseup(e, button);
+        }, {once: true});
+    }
+    
+    function mouseup(e, button){
+        if(e.target.classList.contains('down')){
+            e.target.classList.toggle('down');
+            calculator.evaluate(button);
         }
-        document.querySelector('.numbers').appendChild(button);
-    });
+        removeListeners(button);
+    }
+    
+    function mouseleave(e){
+        e.target.classList.toggle('down');
+        e.target.addEventListener('mouseenter', mouseenter);
+    }
+
+    function mouseenter(e){
+        e.target.classList.toggle('down');
+    }
+
+    function removeListeners(button){
+        button.removeEventListener('mouseleave', mouseleave);
+        button.removeEventListener('mouseenter', mouseenter);
+    }
+
+    return this.div;
 }
 
-function buttonListeners(){
-    let numbers = document.querySelectorAll('.numbers > div');
-    let operators = document.querySelectorAll('.operators > div');
-
-    addListeners(numbers);
-    addListeners(operators);
-}
-
-function addListeners(buttons){
-    buttons.forEach(button => button.addEventListener('mousedown', mousedown));
-    buttons.forEach(button => button.addEventListener('mouseup', mouseup));
-}
-
-function mouseup(e){
-    console.log(e.target);
-    e.target.classList.toggle('down');
-    handler(e.target.textContent);
-}
-
-function mousedown(e){
-    e.target.classList.toggle('down');
-    e.target.addEventListener('mouseleave', mouseleave);
-}
-
-function mouseleave(e){
-
-}
-
-function handler(button){
-    console.log(button);
-}
-
-constructCalculator();
-buttonListeners();
+let calculator = new Calculator();
+calculator.fill();
