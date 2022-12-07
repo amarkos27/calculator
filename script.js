a42b6ce25c4231265fe9fda31e186f0d70b16db3
amarkos27 <armarkos27@gmail.com>
2022-12-07 16:14:29 -0500

    Modify display behavior
    As it stood previously, the display function only progressively reduced
    the font size of the expression with the font size only resetting when
    the clear button was pressed. Replace this with a mutationObserver that
    sizes the font relative to the number of digits that will fit in the
    available space.

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 function Calculator(){
     this.numbers = ['Clear', 7, 8, 9, 4, 5, 6, 1, 2, 3, 0, '.'];
     this.operators = ['÷','×','-','+', '='];
-    this.content = document.querySelector('.contentWrapper');
-    this.font_size = window.getComputedStyle(this.content).fontSize;
     this.expression = '';
+    this.screen = document.querySelector('.screen');
 
     this.fill = function(){
         this.operators.forEach(operator => {
@@ -15,7 +14,6 @@ function Calculator(){
         });
         this.numbers.forEach(number => {
             let button = new Button(number);
-            button.textContent = number;
             switch(button.textContent){
                 case 'Clear':
                     button.style.gridColumn = '1/4';
@@ -28,118 +26,166 @@ function Calculator(){
             document.querySelector('.numbers').appendChild(button);
         });
     };
-    
-    this.display = function(){
-        if(this.expression != ''){ //Only modify font if expression is not empty
-            let num = this.font_size.split('').filter(char => /[\d\.]/.test(char)).join('');
-            if(this.content.getBoundingClientRect().width >= 
-            this.content.parentNode.getBoundingClientRect().width - 40){
-                num *= 0.8;
-            }
-            this.font_size = `${num}px`;
-            this.content.style.fontSize = this.font_size;
-        }
-        this.content.textContent = this.expression;
-    };
 
-    this.handler = function(button){
-        if(button.textContent === 'Clear'){
-            this.expression = '';
-            this.font_size = '50px';
-        }else if(button.parentNode.classList.contains('numbers')){
-            this.expression += button.textContent;
-        }else{ //if button is an operator
-            if(this.expression !== ''){
-                //Discount first - sign if operand is negative
-                let operator = this.expression.split('').find((char, index) => /[÷×\-\+]/.test(char) && index > 0);
-                if(operator){
-                    this.evaluate(operator);
-                }
-                if(button.textContent !== '='){
-                    this.expression += button.textContent;
-                }
-            }
-        }
-        this.display();
-    };
-
-    this.evaluate = function(operator){
-        let splitPoint = this.expression.split('').findIndex((char, index) => /[÷×\-\+]/.test(char) && index > 0);
-        let operands = [this.expression.slice(0, splitPoint), this.expression.slice(splitPoint + 1)];
-        console.log(operands);
-        switch(operator){
-            case '+':
-                this.expression = add(operands);
-                break;
-            case '-':
-                this.expression = subtract(operands);
-                break;
-            case '×':
-                this.expression = multiply(operands);
-                break;
-            case '÷':
-                this.expression = divide(operands);
-                break;
-            default:
-                this.expression = 'ERROR';
-        }
-    };
-
-    function add(operands){
-        return String(+operands[0] + +operands[1]);
+    this.add = function(operands){
+        return String(Math.round(((+operands[0] + +operands[1]) + Number.EPSILON) * 100000) / 100000);
     }
 
-    function subtract(operands){
-        return String(operands[0] - operands[1]);
+    this.subtract = function(operands){
+        return String(Math.round(((operands[0] - operands[1]) + Number.EPSILON) * 100000) / 100000);
     }
 
-    function multiply(operands){
-        return String(operands[0] * operands[1]);
+    this.multiply = function(operands){
+        return String(Math.round(((operands[0] * operands[1]) + Number.EPSILON) * 100000) / 100000);
     }
 
-    function divide(operands){
-        return String(operands[0] / operands[1]);
+    this.divide = function(operands){
+        return String(Math.round(((operands[0] / operands[1]) + Number.EPSILON) * 100000) / 100000);
     }
-}
-function Button(button){
-    this.div = document.createElement('div');
-    this.div.textContent = button;
-    
-    this.div.addEventListener('mousedown', mousedown);
-
-    function mousedown(e){
-        let button = e.target;
-        button.classList.toggle('down');
-        button.addEventListener('mouseleave', mouseleave);
-        window.addEventListener('mouseup', (e) => {
-            mouseup(e, button);
-        }, {once: true});
-    }
-    
-    function mouseup(e, button){
-        if(e.target.classList.contains('down')){
-            e.target.classList.toggle('down');
-            calculator.handler(button);
-        }
-        removeListeners(button);
-    }
-    
-    function mouseleave(e){
-        e.target.classList.toggle('down');
-        e.target.addEventListener('mouseenter', mouseenter);
-    }
-
-    function mouseenter(e){
-        e.target.classList.toggle('down');
-    }
-
-    function removeListeners(button){
-        button.removeEventListener('mouseleave', mouseleave);
-        button.removeEventListener('mouseenter', mouseenter);
-    }
-
-    return this.div;
 }
 
 let calculator = new Calculator();
 calculator.fill();
+
+calculator.display = function(){
+    calculator.screen.textContent = calculator.expression;
+};
+
+/** Resize font based on window space **/
+let screenCover = document.querySelector('.screen-cover');
+let displaySize;
+
+let config = {
+    attributes: true,
+    childList: true,
+    characterData: true,
+};
+
+let displayObserver = new MutationObserver(function(mutations){
+    displaySize = 100;
+    screenCover.style.cssText = 'font-size:' + displaySize + '%';
+
+    while(calculator.screen.offsetWidth > screenCover.offsetWidth - 20){
+        displaySize -= 1;
+        screenCover.style.cssText = 'font-size:' + displaySize + '%';
+    }
+});
+
+displayObserver.observe(calculator.screen, config);
+/***************************************/
+
+calculator.handler = function(button){
+    if(button.textContent === 'Clear'){
+        calculator.expression = '';
+    }else if(button.parentNode.classList.contains('numbers')){
+        //Replace content on screen (instead of adding) with number pressed if
+        //content on screen is error message or just a 0
+        if(calculator.expression === 'ERROR, Divide by 0' || 
+        (calculator.getOperands()[0] === '0' && button.textContent !== '.')
+        ){
+            calculator.expression = button.textContent;
+        }else if(calculator.getOperands()[1] === '0' && button.textContent !== '.'){
+            //Replace second operand with number pressed if it is a 0 not followed by a decimal
+            let replace = calculator.expression.split(calculator.getOperator());
+            replace[1] = button.textContent;
+            replace = replace.join(calculator.getOperator());
+            calculator.expression = replace;
+        }
+        else{
+            //If no special case, add number to expression
+            calculator.expression += button.textContent;
+        }
+    }else{
+        let operator = calculator.getOperator();
+        if(operator){
+            calculator.evaluate(operator);
+        }
+        if(button.textContent !== '=' && calculator.expression !== ''){
+            calculator.expression += button.textContent;
+        }
+
+    }
+    calculator.display();
+};
+
+calculator.evaluate = function(operator){
+    let operands = calculator.getOperands();
+    switch(operator){
+        case '+':
+            calculator.expression = calculator.add(operands);
+            break;
+        case '-':
+            calculator.expression = calculator.subtract(operands);
+            break;
+        case '×':
+            calculator.expression = calculator.multiply(operands);
+            break;
+        case '÷':
+            if(operands[1] !== '0'){
+                calculator.expression = calculator.divide(operands);
+            }else{
+                calculator.expression = 'ERROR, Divide by 0'
+            }
+            break;
+        default:
+            calculator.expression = 'ERROR';
+    }
+};
+
+calculator.getOperator = function(){
+    return calculator.expression.split('').find((char, index) => /[÷×\-\+]/.test(char) && index > 0);
+}
+
+calculator.getOperands = function(){
+    let operator = calculator.getOperator();
+    let operands = null;
+    if(operator){
+        //Skip over first - sign if negative
+        let splitPoint = calculator.expression.split('')
+        .findIndex((char, index) => char === operator && index > 0);
+
+        operands = 
+        [calculator.expression.slice(0, splitPoint), calculator.expression.slice(splitPoint + 1)];
+    }else{
+        operands = calculator.expression;
+    }
+    return operands;
+}
+
+function Button(button){
+    this.div = document.createElement('div');
+    this.div.textContent = button;
+    this.div.addEventListener('mousedown', mousedown);
+
+    return this.div;
+}
+
+function mousedown(e){
+    let button = e.target;
+    button.classList.toggle('down');
+    button.addEventListener('mouseleave', mouseleave);
+    window.addEventListener('mouseup', (e) => {
+        mouseup(e, button);
+    }, {once: true});
+}
+
+function mouseup(e, button){
+    if(e.target.classList.contains('down')){
+        e.target.classList.toggle('down');
+        calculator.handler(button);
+    }
+    removeListeners(button);
+}
+
+function mouseleave(e){
+    e.target.classList.toggle('down');
+    e.target.addEventListener('mouseenter', mouseenter, {once: true});
+}
+
+function mouseenter(e){
+    e.target.classList.toggle('down');
+}
+
+function removeListeners(button){
+    button.removeEventListener('mouseleave', mouseleave);
+}
