e325cd1ab3d28ee6bfc6e5ec5ad81025cc9b6e3a
amarkos27 <armarkos27@gmail.com>
2023-03-23 18:43:38 -0400

    Fix formatting

--- script.js
+++ script.js
@@ -1,49 +1,52 @@
-let calc = {expression: '0'};
-let buttons = document.querySelectorAll('.numbers > div, .operators > div');
-let screen = document.querySelector('.screen');
+let calc = { expression: "0" };
+let buttons = document.querySelectorAll(".numbers > div, .operators > div");
+let screen = document.querySelector(".screen");
 let isOperator = /[\+÷\-×]/;
 let isNumber = /\d/;
-    
 
 display();
 
-buttons.forEach(button => {
-    button.addEventListener('mousedown', mousedown);
+buttons.forEach((button) => {
+  button.addEventListener("mousedown", mousedown);
 });
 
 // Event listeners for proper button clicking functionality
-function mousedown(e){
-    let button = e.target;
-    button.classList.toggle('down');
-    button.addEventListener('mouseleave', mouseleave);
-    window.addEventListener('mouseup', (e) => {
-        mouseup(e, button);
-    }, {once: true});
+function mousedown(e) {
+  let button = e.target;
+  button.classList.toggle("down");
+  button.addEventListener("mouseleave", mouseleave);
+  window.addEventListener(
+    "mouseup",
+    (e) => {
+      mouseup(e, button);
+    },
+    { once: true }
+  );
 }
 
-function mouseleave(e){
-    let button = e.target;
-    button.classList.toggle('down');
-    button.addEventListener('mouseenter', mouseenter, {once: true});
+function mouseleave(e) {
+  let button = e.target;
+  button.classList.toggle("down");
+  button.addEventListener("mouseenter", mouseenter, { once: true });
 }
 
-function mouseenter(e){
-    e.target.classList.toggle('down');
+function mouseenter(e) {
+  e.target.classList.toggle("down");
 }
 
 //Flow will start here when a button has actually been pressed
-function mouseup(e, button){
-    removeListeners(button);
-    if(e.target.parentNode.parentNode.classList.contains('buttons')){
-        e.target.classList.toggle('down');
-        validator(e.target.textContent);
-        display();
-    }
+function mouseup(e, button) {
+  removeListeners(button);
+  if (e.target.parentNode.parentNode.classList.contains("buttons")) {
+    e.target.classList.toggle("down");
+    validator(e.target.textContent);
+    display();
+  }
 }
 
-function removeListeners(button){
-    button.removeEventListener('mouseleave', mouseleave);
-    button.removeEventListener('mouseenter', mouseenter, {once: true});
+function removeListeners(button) {
+  button.removeEventListener("mouseleave", mouseleave);
+  button.removeEventListener("mouseenter", mouseenter, { once: true });
 }
 
 /* Validator Function
@@ -52,157 +55,153 @@ function removeListeners(button){
     have occured. If so, the proper changes will be made. If not, the display function is called
     to display the current state of the expression
 */
-function validator(pressed){
-    let recentChar = calc.expression[calc.expression.length - 1]
-    if(pressed === 'Clear'){
-        calc.expression = '0';
-        return;
+function validator(pressed) {
+  let recentChar = calc.expression[calc.expression.length - 1];
+  if (pressed === "Clear") {
+    calc.expression = "0";
+    return;
+  }
+  if (calc.expression === "ERROR, divide by 0") {
+    if (isNumber.test(pressed)) {
+      calc.expression = pressed;
+      return;
+    } else if (isOperator.test(pressed)) {
+      return;
     }
-    if(calc.expression === 'ERROR, divide by 0'){
-        if(isNumber.test(pressed)){
-            calc.expression = pressed;
-            return;
-        }else if (isOperator.test(pressed)){
-            return;
-        }
+  } else if (firstDigitIsZero() && isNumber.test(pressed)) {
+    replace(pressed);
+  } else if (pressed === ".") {
+    //Only add decimal if previous character is a number
+    if (!decimalPresent() && isNumber.test(recentChar)) {
+      calc.expression += ".";
+    } else if (isOperator.test(recentChar)) {
+      calc.expression += "0.";
     }
-    else if(firstDigitIsZero() && isNumber.test(pressed)){
-        replace(pressed);
-    }else if(pressed === '.'){
-        //Only add decimal if previous character is a number
-        if(!decimalPresent() && isNumber.test(recentChar)){
-            calc.expression += '.';
-        }else if(isOperator.test(recentChar)){
-            calc.expression += '0.'
-        }
-    }else if(isOperator.test(pressed)){
-        if(getOperands().length === 2){
-            evaluate();
-            calc.expression += pressed;
-        }else if(getOperator() && getOperands().length === 1){
-            replace(pressed);
-        }else{
-            calc.expression += pressed;
-        }
-    }else if(pressed === '='){
-        if(getOperands().length === 2){
-            evaluate();
-        }
-        else{
-            return;
-        }
+  } else if (isOperator.test(pressed)) {
+    if (getOperands().length === 2) {
+      evaluate();
+      calc.expression += pressed;
+    } else if (getOperator() && getOperands().length === 1) {
+      replace(pressed);
+    } else {
+      calc.expression += pressed;
     }
-    else{
-        calc.expression += pressed;
+  } else if (pressed === "=") {
+    if (getOperands().length === 2) {
+      evaluate();
+    } else {
+      return;
     }
+  } else {
+    calc.expression += pressed;
+  }
 }
 
-function replace(pressed){
-    let replace = calc.expression.split('');
-    replace[replace.length - 1] = pressed;
-    calc.expression = replace.join('');
+function replace(pressed) {
+  let replace = calc.expression.split("");
+  replace[replace.length - 1] = pressed;
+  calc.expression = replace.join("");
 }
 
-function firstDigitIsZero(){
-    let operands = getOperands();
-    let recentOperand = operands[operands.length - 1];
-    if(recentOperand === '0'){
-        return true;
-    }else return false;
+function firstDigitIsZero() {
+  let operands = getOperands();
+  let recentOperand = operands[operands.length - 1];
+  if (recentOperand === "0") {
+    return true;
+  } else return false;
 }
 
-function decimalPresent(){
-    let operands = getOperands();
-    let recentOperand = operands[operands.length - 1];
-    if(recentOperand.includes('.')){
-        return true;
-    }else return false;
+function decimalPresent() {
+  let operands = getOperands();
+  let recentOperand = operands[operands.length - 1];
+  if (recentOperand.includes(".")) {
+    return true;
+  } else return false;
 }
 
-
-function getOperands(){
-    let splitPoint = 0;;
-    for(let i = 1; i < calc.expression.length; i++){
-        if(isOperator.test(calc.expression[i])){
-            splitPoint = i;
-            let operands = [];
-            operands[0] = calc.expression.slice(0, splitPoint);
-            operands[1] = calc.expression.slice(splitPoint + 1);
-            return operands.filter(element => element !== '');
-        }
+function getOperands() {
+  let splitPoint = 0;
+  for (let i = 1; i < calc.expression.length; i++) {
+    if (isOperator.test(calc.expression[i])) {
+      splitPoint = i;
+      let operands = [];
+      operands[0] = calc.expression.slice(0, splitPoint);
+      operands[1] = calc.expression.slice(splitPoint + 1);
+      return operands.filter((element) => element !== "");
     }
-    return [calc.expression];
+  }
+  return [calc.expression];
 }
 
-function getOperator(){
-    for(let i = 1; i < calc.expression.length; i++){
-        if(isOperator.test(calc.expression[i])){
-            return calc.expression[i];
-        }
+function getOperator() {
+  for (let i = 1; i < calc.expression.length; i++) {
+    if (isOperator.test(calc.expression[i])) {
+      return calc.expression[i];
     }
-    return false;
+  }
+  return false;
 }
 
 /* Evaluate function
     Handles all mathematical evaluations and adds result to the expression
 */
-function evaluate(){
-    let operands = getOperands();
-    let operator = getOperator();
-    let result = null;
+function evaluate() {
+  let operands = getOperands();
+  let operator = getOperator();
+  let result = null;
 
-    if(operands[1] === '0.'){
-        operands[1] = '0';
+  if (operands[1] === "0.") {
+    operands[1] = "0";
+  }
+
+  if (operator === "+") {
+    result = +operands[0] + +operands[1];
+    calc.expression = round(result);
+  } else if (operator === "-") {
+    result = operands[0] - operands[1];
+    calc.expression = round(result);
+  } else if (operator === "×") {
+    result = operands[0] * operands[1];
+    calc.expression = round(result);
+  } else if (operator === "÷") {
+    if (operands[1] === "0") {
+      calc.expression = "ERROR, divide by 0";
+      return;
     }
-    
-    if(operator === '+'){
-        result = +operands[0] + +operands[1];
-        calc.expression = round(result);
-    }else if(operator === '-'){
-        result = operands[0] - operands[1];
-        calc.expression = round(result);
-    }else if(operator === '×'){
-        result = operands[0] * operands[1];
-        calc.expression = round(result);
-    }else if(operator === '÷'){
-        if(operands[1] === '0'){
-            calc.expression = 'ERROR, divide by 0';
-            return;
-        }
-        result = operands[0] / operands[1];
-        calc.expression = round(result);
-    }
+    result = operands[0] / operands[1];
+    calc.expression = round(result);
+  }
 }
 
-function round(num){
-    return String(Math.round((num + Number.EPSILON) * 100000) / 100000);
+function round(num) {
+  return String(Math.round((num + Number.EPSILON) * 100000) / 100000);
 }
 
 /* Display Function
     Displays the current expression after it has been properly evaluated according to special cases
 */
-function display(){
-    screen.textContent = calc.expression;
+function display() {
+  screen.textContent = calc.expression;
 }
 
 /** Resize font based on window space **/
-let screenCover = document.querySelector('.screen-cover');
+let screenCover = document.querySelector(".screen-cover");
 let displaySize;
 
 let config = {
-    attributes: true,
-    childList: true,
-    characterData: true,
+  attributes: true,
+  childList: true,
+  characterData: true,
 };
 
-let displayObserver = new MutationObserver(function(mutations){
-    displaySize = 100;
-    screenCover.style.cssText = 'font-size:' + displaySize + '%';
+let displayObserver = new MutationObserver(function (mutations) {
+  displaySize = 100;
+  screenCover.style.cssText = "font-size:" + displaySize + "%";
 
-    while(screen.offsetWidth > screenCover.offsetWidth - 20){
-        displaySize -= 1;
-        screenCover.style.cssText = 'font-size:' + displaySize + '%';
-    }
+  while (screen.offsetWidth > screenCover.offsetWidth - 20) {
+    displaySize -= 1;
+    screenCover.style.cssText = "font-size:" + displaySize + "%";
+  }
 });
 
 displayObserver.observe(screen, config);
